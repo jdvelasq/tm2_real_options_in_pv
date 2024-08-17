a25d5c4b0c40f85cb3ffc7faf73550a6384e486b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-08-17 06:58:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-pip3 install -e /Volumes/GitHub/techminer2 --force-reinstall
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install --upgrade pip
+pip3 install -e /Volumes/GitHub/techminer2
+pip3 install black isort ipykernel
